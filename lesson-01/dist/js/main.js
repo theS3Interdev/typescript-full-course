468b7072b17e6706f8339b53f33c7426f00345fa
Michael Owuor <michael.owuor@gmail.com>
2023-07-17 16:50:22 +0300

    initial commit

---
+++ lesson-01/dist/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let userName = 'Michael Owuor';
+console.log(userName);
+let a = 32;
+let b = 6;
+let c = 2;
+console.log(a / b);
+console.log(c * b);
---
+++ lesson-01/src/main.ts
@@ -0,0 +1,11 @@
+let userName = 'Michael Owuor';
+
+console.log(userName);
+
+let a: number = 32;
+let b: number = 6;
+let c: number = 2;
+
+console.log(a / b);
+
+console.log(c * b);
