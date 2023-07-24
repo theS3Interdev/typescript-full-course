6d2e01296f05199d01f098e1cd6120063eb9bef8
Michael Owuor <michael.owuor@gmail.com>
2023-07-24 16:50:58 +0300

    lesson-12b - react + typescript

--- lesson-12/src/App.tsx
+++ lesson-12/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Heading from './components/heading';
 import Section from './components/section';
 import Counter from './components/counter';
+import List from './components/list';
 
 function App() {
 	const [count, setCount] = useState<number>(1);
@@ -11,6 +12,10 @@ function App() {
 			<Heading title={'Hello'} />
 			<Section title={'Different Title'}>This is my Section.</Section>
 			<Counter setCount={setCount}>Count is {count}</Counter>
+			<List
+				items={['☕ Coffee', '🌮 Tacos', '💻 Code']}
+				render={(item: string) => <span className="bold">{item}</span>}
+			/>
 		</>
 	);
 }
